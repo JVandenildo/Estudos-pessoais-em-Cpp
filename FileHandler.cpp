803f692fd324499227b953c6cb6f5f5c9e0a1030
J. Vandenildo <jvandenildo@gmail.com>
2023-09-19 07:57:44 -0300

    minor changes

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -3,34 +3,42 @@
 #include <string>
 using namespace std;
 
-// int main(){
+// int main()
+// {
 //     fstream myFile;
 
-//     // myFile.open("testFile.txt", ios::out); // write mode
-//     // // receives 2 parameters: file's name, how to open that file, if the opened file does not exist it will be created
-//     // if(myFile.is_open()){
-//     //     myFile << "Hello.\n";
-//     //     myFile << "Second line.\n";
+//     myFile.open("testFile.txt", ios::out); // write mode
+//     // receives 2 parameters: file's name, how to open that file, if the opened file does not exist it will be created
 
-//     //     myFile.close(); // a must after use a file
-//     // }
+//     if (myFile.is_open())
+//     {
+//         myFile << "Hello.\n";
+//         myFile << "Second line.\n";
+
+//         myFile.close(); // a must after use a file
+//     }
+
 //     // myFile.open("testFile.txt", ios::app); // write mode
 //     // // out overwrites the file, app appends new content
-//     // if(myFile.is_open()){
+
+//     // if (myFile.is_open())
+//     // {
 //     //     myFile << "Hello 2!\n";
-        
+
 //     //     myFile.close(); // a must after use a file
 //     // }
 
-//     myFile.open("testFile.txt", ios::in); // read mode
-//     if(myFile.is_open()){
-//         string line;
-//         while(getline(myFile, line)){
-//             cout << line << endl;
-//         }
+//     // myFile.open("testFile.txt", ios::in); // read mode
+//     // if (myFile.is_open())
+//     // {
+//     //     string line;
+//     //     while (getline(myFile, line))
+//     //     {
+//     //         cout << line << endl;
+//     //     }
 
-//         myFile.close();
-//     }
+//     //     myFile.close();
+//     // }
 
 //     return 0;
 // }
---
+++ Object&Classes/FunctionsAndOop.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+/* virtual function is defined in base class
+and redefined in derived */
+class Instrument
+{
+public:
+    virtual void makeSound() = 0; // pure virtual function
+};
+
+class Accordion : public Instrument
+{
+public:
+    void makeSound()
+    {
+        std::cout << "Accordion playing...\n";
+    }
+};
+
+class Piano : public Instrument
+{
+public:
+    void makeSound()
+    {
+        std::cout << "Piano playing...\n";
+    }
+};
+
+int main()
+{
+    Instrument *i1 = new Accordion();
+    // i1->makeSound();
+    Instrument *i2 = new Piano();
+    // i2->makeSound();
+
+    Instrument *instruments[2] = {i1, i2};
+    for (int i = 0; i < 2; i++)
+        instruments[i]->makeSound();
+
+    return 0;
+}
